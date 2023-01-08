cbfa4c95a6f1b1cfb3cdf7af9ca0f7902d2d479f
Wojtek F <fedakwojciech@gmail.com>
2023-01-08 20:15:29 +0100

    add hooks too usersList

--- src/components/organisms/usersList/UsersList.js
+++ src/components/organisms/usersList/UsersList.js
@@ -1,16 +1,29 @@
-import React from "react";
-import { users } from "../../../data/users";
+import React, { useState } from "react";
+import { users as userData } from "../../../data/users";
 import UsersListItem from "../../molecues/usersListItem/UsersListItem";
 import { Wrapper, StyledList } from "./UsersList.styles";
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData) => (
-        <UsersListItem userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+const UsersList = () => {
+  const [users, setUsers] = useState(userData);
+
+  const deleteUsers = (name) => {
+    const filtersUsers = users.filter((user) => user.name !== name);
+    setUsers(filtersUsers);
+  };
+
+  return (
+    <Wrapper>
+      <StyledList>
+        {users.map((userData) => (
+          <UsersListItem
+            userData={userData}
+            key={userData.name}
+            deleteUser={deleteUsers}
+          />
+        ))}
+      </StyledList>
+    </Wrapper>
+  );
+};
 
 export default UsersList;
