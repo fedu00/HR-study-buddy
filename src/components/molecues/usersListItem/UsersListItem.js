1db60f3a0cf201f4c1b3997f0a48a8d52f725898
Wojtek F <fedakwojciech@gmail.com>
2023-01-14 13:28:16 +0100

    add react context

--- src/components/molecues/usersListItem/UsersListItem.js
+++ src/components/molecues/usersListItem/UsersListItem.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import DeleteButton from "../../atoms/deleteButton/DeleteButton";
 import { Wrapper } from "./UserListItem.styles";
 import UserAverageItem from "../../atoms/userAverageItem/UserAverageItem";
 import { UserShape } from "../../../assets/types/types";
+import { UsersContext } from "../../../providers/UsersProvider";
 
-const UsersListItem = ({
-  deleteUser,
-  userData: { name, average, attendance },
-}) => (
-  <Wrapper>
-    <UserAverageItem average={average} />
-    <div>
-      <p>{name}</p>
-      <p>attendance {attendance}</p>
-    </div>
-    <DeleteButton onClick={() => deleteUser(name)} />
-  </Wrapper>
-);
+const UsersListItem = ({ userData: { name, average, attendance } }) => {
+  const { deleteUser } = useContext(UsersContext);
+
+  return (
+    <Wrapper>
+      <UserAverageItem average={average} />
+      <div>
+        <p>{name}</p>
+        <p>attendance {attendance}</p>
+      </div>
+      <DeleteButton onClick={() => deleteUser(name)} />
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape(UserShape),
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,77 +1,29 @@
 // import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import UsersList from "./usersList/UsersList";
 import { ThemeProvider } from "styled-components";
 import { Wrapper } from "./Rood.styles";
 import { GloballStyle } from "../assets/icons/styles/GlobalStyle";
 import { theme } from "../assets/icons/styles/theme";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainTemplate from "../components/templates/MainTemplate";
-import { users as userData } from "../data/users";
-import Form from "./Form/Form";
-
-const inicialFormState = {
-  name: "",
-  attendance: "",
-  average: "",
-};
+import AddUserForm from "./Form/AddUserForm";
+import UsersProvider from "../providers/UsersProvider";
 
 const Root = () => {
-  const [users, setUsers] = useState(userData);
-  const [formValue, setFormValueformValue] = useState(inicialFormState);
-  // const location = useLocation();
-
-  const deleteUsers = (name) => {
-    const filtersUsers = users.filter((user) => user.name !== name);
-    setUsers(filtersUsers);
-  };
-
-  const handleInputChange = (e) => {
-    setFormValueformValue({
-      ...formValue,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const newUser = {
-      name: formValue.name,
-      attendance: formValue.attendance,
-      average: formValue.average,
-    };
-    setUsers([newUser, ...users]);
-    setFormValueformValue(inicialFormState);
-  };
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GloballStyle />
         <MainTemplate>
-          <Wrapper>
-            <Routes>
-              <Route
-                path="/"
-                element={<UsersList deleteUsers={deleteUsers} users={users} />}
-              />
-              <Route
-                path="/add-user"
-                element={
-                  <Form
-                    formValue={formValue}
-                    handleAddUser={handleAddUser}
-                    handleInputChange={handleInputChange}
-                  />
-                }
-              />
-            </Routes>
-          </Wrapper>
+          <UsersProvider>
+            <Wrapper>
+              <Routes>
+                <Route path="/" element={<UsersList />} />
+                <Route path="/add-user" element={<AddUserForm />} />
+              </Routes>
+            </Wrapper>
+          </UsersProvider>
         </MainTemplate>
       </ThemeProvider>
     </Router>
