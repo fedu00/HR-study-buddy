8f7497a66c8c48236c6e235edc89e0d58a4313b8
Wojtek F <fedakwojciech@gmail.com>
2023-01-18 18:49:36 +0100

    add hooks

---
+++ src/hooks/useForm.js
@@ -0,0 +1,66 @@
+import { useReducer } from "react";
+
+const actionType = {
+  inputChange: "IPUT CHANGE",
+  clearValue: "CLEAR VALUE",
+  consentToggle: "CONSENT TOGGLE",
+  throwError: "THROW ERROR",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionType.inputChange:
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case actionType.clearValue:
+      return { ...action.initialValues };
+    case actionType.consentToggle:
+      return {
+        ...state,
+        consent: !state.consent,
+      };
+    case actionType.throwError:
+      return {
+        ...state,
+        error: action.errorValue,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useForm = (initialValues) => {
+  const [formValues, dispatch] = useReducer(reducer, initialValues);
+
+  const handleInputChange = (e) => {
+    dispatch({
+      type: actionType.inputChange,
+      field: e.target.name,
+      value: e.target.value,
+    });
+  };
+
+  const handleClearForm = () => {
+    dispatch({ type: actionType.clearValue, initialValues });
+  };
+
+  const handleThrowError = (errorMessage) => {
+    dispatch({
+      type: actionType.throwError,
+      errorValue: errorMessage,
+    });
+  };
+
+  const handleToggleConsent = () => {
+    dispatch({ type: actionType.consentToggle });
+  };
+  return {
+    formValues,
+    handleInputChange,
+    handleClearForm,
+    handleThrowError,
+    handleToggleConsent,
+  };
+};
